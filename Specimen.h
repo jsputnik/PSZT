45ea6261d1fda2e42a4553636d1e5ae926c6af32
wshknmt <01143561@pw.edu.pl>
2020-11-15 19:01:01 +0100

    added class Specimen

---
+++ Specimen.h
@@ -0,0 +1,13 @@
+#ifndef SPECIMEN_H_INCLUDED
+#define SPECIMEN_H_INCLUDED
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Specimen
+{
+    vector < bool > element;
+};
+
+#endif // SPECIMEN_H_INCLUDED
