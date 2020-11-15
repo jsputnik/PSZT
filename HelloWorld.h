6982efa09d92ca1a1e70aff15c0e00d41d613bcf
jsputnik <65297977+jsputnik@users.noreply.github.com>
2020-11-15 18:12:53 +0100

    Add files via upload

---
+++ HelloWorld.h
@@ -0,0 +1,11 @@
+#ifndef HelloWorld_h
+#define HelloWorld_h
+#include <iostream>
+
+using namespace std;
+
+void hello_world() {
+    cout << "Hello World" << endl;
+}
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "HelloWorld.h"
+
+using namespace std;
+
+int main() {
+    int pm = 0;
+    int pc = 0;
+    int mu = 0;
+    int t = 0;
+    hello_world();
+    /*p = initialise();
+    grade(p0);
+    while(!end) {
+        t = selection(p);
+        o = crossing_and_mutation(t);
+        garde(o);
+        p = o;
+        ++t;
+    }*/
+}
