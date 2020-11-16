c616675bcc9f9a62bb8a7fd6cee604171b404e8a
jsputnik <65297977+jsputnik@users.noreply.github.com>
2020-11-16 22:37:14 +0100

    Update Specimen.cpp

--- Specimen.cpp
+++ Specimen.cpp
@@ -1,5 +1,7 @@
 #include "Specimen.h"
+#include <cmath>
 
+using namespace std;
 /*****************************************
  _______________________________
 |Card Value  | 1 | 2 | ... | N  |
@@ -7,18 +9,67 @@
 |A (0) B (1) | 0 | 1 | ... | 1  |
 |____________|___|___|_____|____|
 *****************************************/
-Specimen::Specimen(int card_quantity) {
+Specimen::Specimen(int card_quantity, double sum_a_goal, double product_b_goal, double match_level_goal) {
     for (int i = 0; i < card_quantity; ++i) {
         int value = rand() % 2;
         element.push_back(value);
     }
+    calculate(sum_a_goal, product_b_goal, match_level_goal);
+}
+
+//making a special case divisions, all cards on A (a = 0) or all on B (a = 1)
+Specimen::Specimen(int card_quantity, int a, double sum_a_goal, double product_b_goal, double match_level_goal) {
+    if (a == 0) {
+        for (int i = 0; i < card_quantity; ++i) {
+            element.push_back(0);
+        }
+    }
+    else if (a == 1) {
+        for (int i = 0; i < card_quantity; ++i) {
+            element.push_back(1);
+        }
+    }
+    else {
+        for (int i = 0; i < card_quantity; ++i) {
+            int value = rand() % 2;
+            element.push_back(value);
+        }
+    }
+    calculate(sum_a_goal, product_b_goal, match_level_goal);
 }
 
 void Specimen::print() {
-    for (int i = 0; i <element.size(); ++i) {
-        std::cout << element[i] << "|";
+    for (unsigned int i = 0; i < element.size(); ++i) {
+        cout << element[i] << "|";
     }
-    std::cout << std:: endl;
+    cout << " ";
+    cout << "Sum A: " << sum_a << ", ";
+    cout << "Product B: " << product_b << ", ";
+    cout << "Match level A: " << match_level_a << ", ";
+    cout << "Match level B: " << match_level_b << ", ";
+    cout << "Grade: " << grade << endl;
 }
 
+void Specimen::calculate_scores() {
+    sum_a = 0;
+    product_b = 0;
+    for (unsigned int i = 0; i < element.size(); ++i) {
+        if (!element[i]) {
+            sum_a += i + 1;
+        }
+        else {
+            product_b = (product_b > 0) ? product_b * (i + 1) : (i + 1);
+        }
+    }
+}
+//the closer to 0 the better
+void Specimen::calculate_match_levels(double sum_a_goal, double product_b_goal, double match_level_goal) {
+    match_level_a = abs(sum_a_goal - sum_a)/sum_a_goal;
+    match_level_b = abs(product_b_goal - product_b)/product_b_goal;
+    grade = max(match_level_a, match_level_b);
+}
 
+void Specimen::calculate(double sum_a_goal, double product_b_goal, double match_level_goal) {
+    calculate_scores();
+    calculate_match_levels(sum_a_goal, product_b_goal, match_level_goal);
+}
